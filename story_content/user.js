10506b942336e7382fae5efee45f86438a4c6fdc
Raja-arch1 <damardanuartha0@gmail.com>
2024-12-27 19:26:50 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5Ziugl08lvi":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+
+audio.src = "musikku.mp3";
+
+audio.load();
+
+audio.play();
+
+audio.volume=0.5;
+}
+
